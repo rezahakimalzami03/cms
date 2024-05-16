1527b709e4b78237128636f799499690e7d22d95
rezahakimalzami03 <rezahakimalzami03@gmail.com>
2024-05-16 13:03:10 +0700

    update postgree

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,16 +1,22 @@
 /* eslint-disable linebreak-style */
-/* eslint-disable indent */
+/* eslint-disable no-unused-vars */
 /* eslint-disable linebreak-style */
+/* eslint-disable indent */
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-            host: env('DATABASE_HOST', '127.0.0.1'),
-            port: env.int('DATABASE_PORT', 5432),
-            database: env('DATABASE_NAME', 'strapi'),
-            user: env('DATABASE_USERNAME', 'strapi'),
-            password: env('DATABASE_PASSWORD', 'strapi'),
-            ssl: env.bool('DATABASE_SSL', false),
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            user: config.user,
+            password: config.password,
+            ssl: {
+                rejectUnauthorized: false
+            },
         },
+        debug: false,
     },
 });
