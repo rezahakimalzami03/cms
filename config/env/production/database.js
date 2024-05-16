5019fdec72a360c7f4e869a74993740eb71522c9
rezahakimalzami03 <rezahakimalzami03@gmail.com>
2024-05-16 16:54:49 +0700

    test maintenance

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,22 +1,27 @@
 /* eslint-disable linebreak-style */
-/* eslint-disable no-unused-vars */
+/* eslint-disable quotes */
 /* eslint-disable linebreak-style */
 /* eslint-disable indent */
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-module.exports = ({ env }) => ({
-    connection: {
-        client: 'postgres',
+/* eslint-disable linebreak-style */
+// path: ./config/env/production/database.js
+
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+    const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+    return {
         connection: {
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            user: config.user,
-            password: config.password,
-            ssl: {
-                rejectUnauthorized: false
+            client: 'postgres',
+            connection: {
+                host,
+                port,
+                database,
+                user,
+                password,
+                ssl: { rejectUnauthorized: false },
             },
+            debug: false,
         },
-        debug: false,
-    },
-});
+    };
+};
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,15 +1,21 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable quotes */
 /* eslint-disable indent */
 /* eslint-disable linebreak-style */
+// Path: ./config/env/production/server.js
+// starting from Strapi v 4.6.1 server.js has to be the following
+
 module.exports = ({ env }) => ({
-    url: env('MY_VERCEL_URL'),
     proxy: true,
+    host: "0.0.0.0",
+    port: process.env.PORT,
+    url: env('MY_HEROKU_URL'),
     app: {
         keys: env.array('APP_KEYS')
     },
     admin: {
         auth: {
-            secret: env('ADMIN_JWT_SECRET')
-        }
-    }
+            secret: env('ADMIN_JWT_SECRET'),
+        },
+    },
 });
